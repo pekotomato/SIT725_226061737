b9ebd6cbba45348f662a1fa95785ad8acb5a3a98
pekotomato <osita425nei@gmail.com>
2026-03-12 21:49:35 +1100

    Keep only 1.3P for submission

--- test2/server.js
+++
@@ -1,7 +0,0 @@
-var express = require("express") ///require is a function in node.js, express is a web framework
-var app = express() ///var app represents web server application
-var port = process.env.port || 3000; ///where we define the port number
- 
-app.listen(port, () => { ///start listening, start the server
-    console.log("App listening to: " + port)
-})
